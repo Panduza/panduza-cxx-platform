192b63bda1165f045793d82203e32faac3825b46
Valentin <valentin.qiu@elsys-design.com>
2022-08-24 15:08:48 +0200

    switch back paho to ssl

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,8 +1,8 @@
 
 FetchContent_Declare(
     paho.mqtt.c
-    GIT_REPOSITORY https://github.com/eclipse/paho.mqtt.c.git
-    GIT_TAG v1.3.8
+    GIT_REPOSITORY https://github.com/Valossy/paho.mqtt.c.git
+    # GIT_TAG v1.3.8
 )
 
 FetchContent_Declare(
