77f8362e6e33ba83e40a15a2f2a9e3ba3f59587e
Valentin <valentin.qiu@elsys-design.com>
2022-10-03 15:48:07 +0200

    add documentation

--- src/meta_drivers/meta_driver_io_fake.hxx
+++ src/meta_drivers/meta_driver_io_fake.hxx
@@ -4,26 +4,33 @@
 
 #include "../meta_driver.hxx"
 
-/// Fake io meta driver for testing
+/// @brief Fake io meta driver for testing
 class MetaDriverIoFake : public MetaDriver
 {
 public:
     /// Destructor
     ~MetaDriverIoFake() ;
+
     /// Constructor
     MetaDriverIoFake() { LOG_F(9, "Meta Driver Io Fake Constructor"); };
-    /// Setup the fake io meta driver
+
+    /// @brief Setup the fake io meta driver
     void setup();
-    /// Send info to the broker when receiving * in "pza" topic
+
+    /// @brief Send info to the broker when receiving * in "pza" topic
     void sendInfo();
+
     /// Auto toggle IO
     void autoToggle();
+    
     /// Message arrived from the mqtt broker
     /// @param msg message received
     void message_arrived(mqtt::const_message_ptr msg) override;
+    
     /// Set the direction of the IO
     /// @param direction direction of the IO to set
     void setDirection(std::string direction) { mDirection = direction; };
+    
     /// set the value of the IO
     /// @param value value of the IO to set
     void setValue(int value) { mValue = value; };
