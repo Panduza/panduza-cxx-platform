ce18e86fc223afa51d0028117e1c79eb0c70af8d
Valentin <valentin.qiu@elsys-design.com>
2022-11-03 13:31:34 +0100

    check if module is loaded

--- start-platform.sh
+++ start-platform.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-rmmod ftdi_sio
-rmmod usbserial
+if lsmod | grep ftdi_sio &> /dev/null ; then
+    rmmod ftdi_sio
+fi
+if lsmod | grep usbserial &> /dev/null ; then
+    rmmod usbserial
+fi
 
 cd /panduza-cxx-platform/build
 
